8b8c419088c5d2de986a157fd7a13fefc101739d
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-22 22:01:11 -0500

    Declaring Client,declare request to the endpoint, Execute the request and print the content on console

--- FinanceScraperExample/Program.cs
+++ FinanceScraperExample/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Data.SqlClient;
 
 namespace FinanceScraperExample
 {
@@ -10,6 +14,16 @@ namespace FinanceScraperExample
     {
         static void Main(string[] args)
         {
+            // Declaring the client
+            var client = new RestClient("https://api.iextrading.com/1.0");
+
+            // Declare the request to the endpoint
+            var request = new RestRequest("/stock/market/batch?symbols=aapl,fb,tsla,amzn,googl,twtr,csco,ndaq&types=quote", Method.GET);
+
+            // Execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;// raw content as string 
+            Console.WriteLine(content);            
         }
     }
 }
